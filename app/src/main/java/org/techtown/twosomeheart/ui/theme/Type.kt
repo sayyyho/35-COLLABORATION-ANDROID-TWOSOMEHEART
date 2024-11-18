04d04d3594705fbbf4ad922b4f26dfac70efabbf
kangyein9892 <inwon9892@naver.com>
2024-11-19 02:16:17 +0900

    setting/#7: 디자인 시스템 폰트 정의

--- app/src/main/java/org/techtown/twosomeheart/ui/theme/Type.kt
+++ app/src/main/java/org/techtown/twosomeheart/ui/theme/Type.kt
@@ -35,9 +35,148 @@ val Typography = Typography(
     */
 )
 
-//예시입니다리
-// val example = TextStyle( //폰트명 작성하기
-//    fontFamily = FontFamily(Font(R.font.)), 폰트 불러오기
-//    fontSize =,ㅆ
-//    lineHeight =,
-//)
+val head1_b_24 = TextStyle(
+    fontFamily = FontFamily(Font(R.font.notosanskr_bold)),
+    fontSize = 24.sp,
+    fontWeight = FontWeight(700),
+    lineHeight = 36.sp
+)
+
+val head2_r_22 = TextStyle(
+    fontFamily = FontFamily(Font(R.font.notosanskr_regular)),
+    fontSize = 22.sp,
+    fontWeight = FontWeight(400),
+    lineHeight = 36.sp
+)
+
+val head3_b_20 = TextStyle(
+    fontFamily = FontFamily(Font(R.font.notosanskr_bold)),
+    fontSize = 20.sp,
+    fontWeight = FontWeight(700),
+    lineHeight = 30.sp
+)
+
+val head4_b_18 = TextStyle(
+    fontFamily = FontFamily(Font(R.font.notosanskr_bold)),
+    fontSize = 18.sp,
+    fontWeight = FontWeight(700),
+    lineHeight = 48.sp
+)
+val head4_b_18_tight = TextStyle(
+    fontFamily = FontFamily(Font(R.font.notosanskr_bold)),
+    fontSize = 18.sp,
+    fontWeight = FontWeight(700),
+    lineHeight = 48.sp,
+    letterSpacing = (-1).sp
+)
+val head4_m_18 = TextStyle(
+    fontFamily = FontFamily(Font(R.font.notosanskr_medium)),
+    fontSize = 18.sp,
+    fontWeight = FontWeight(500),
+    lineHeight = 48.sp
+)
+val head4_r_18 = TextStyle(
+    fontFamily = FontFamily(Font(R.font.notosanskr_regular)),
+    fontSize = 18.sp,
+    fontWeight = FontWeight(400),
+    lineHeight = 48.sp
+)
+val title1_b_16 = TextStyle(
+    fontFamily = FontFamily(Font(R.font.notosanskr_bold)),
+    fontSize = 16.sp,
+    fontWeight = FontWeight(700),
+    lineHeight = 24.sp
+)
+val title1_r_16 = TextStyle(
+    fontFamily = FontFamily(Font(R.font.notosanskr_regular)),
+    fontSize = 16.sp,
+    fontWeight = FontWeight(400),
+    lineHeight = 24.sp
+)
+val title2_b_15 = TextStyle(
+    fontFamily = FontFamily(Font(R.font.notosanskr_bold)),
+    fontSize = 15.sp,
+    fontWeight = FontWeight(700),
+    lineHeight = 21.sp
+)
+val body1_b_14 = TextStyle(
+    fontFamily = FontFamily(Font(R.font.notosanskr_bold)),
+    fontSize = 14.sp,
+    fontWeight = FontWeight(700),
+    lineHeight = 21.sp
+)
+val body1_b_14_tight = TextStyle(
+    fontFamily = FontFamily(Font(R.font.notosanskr_bold)),
+    fontSize = 14.sp,
+    fontWeight = FontWeight(700),
+    lineHeight = 21.sp,
+    letterSpacing = (-1).sp
+)
+val body1_m_14 = TextStyle(
+    fontFamily = FontFamily(Font(R.font.notosanskr_medium)),
+    fontSize = 14.sp,
+    fontWeight = FontWeight(500),
+    lineHeight = 21.sp
+)
+val body1_r_14 = TextStyle(
+    fontFamily = FontFamily(Font(R.font.notosanskr_regular)),
+    fontSize = 14.sp,
+    fontWeight = FontWeight(400),
+    lineHeight = 21.sp
+)
+val body2_b_13 = TextStyle(
+    fontFamily = FontFamily(Font(R.font.notosanskr_bold)),
+    fontSize = 13.sp,
+    fontWeight = FontWeight(700),
+    lineHeight = 21.sp
+)
+val body2_r_13 = TextStyle(
+    fontFamily = FontFamily(Font(R.font.notosanskr_regular)),
+    fontSize = 13.sp,
+    fontWeight = FontWeight(400),
+    lineHeight = 21.sp
+)
+val caption1_b_12 = TextStyle(
+    fontFamily = FontFamily(Font(R.font.notosanskr_bold)),
+    fontSize = 12.sp,
+    fontWeight = FontWeight(700),
+    lineHeight = 18.sp
+)
+val caption1_m_12 = TextStyle(
+    fontFamily = FontFamily(Font(R.font.notosanskr_medium)),
+    fontSize = 12.sp,
+    fontWeight = FontWeight(500),
+    lineHeight = 18.sp
+)
+val caption1_r_12_tight = TextStyle(
+    fontFamily = FontFamily(Font(R.font.notosanskr_regular)),
+    fontSize = 12.sp,
+    fontWeight = FontWeight(400),
+    lineHeight = 18.sp,
+    letterSpacing = (-0.3).sp
+)
+val caption2_m_11 = TextStyle(
+    fontFamily = FontFamily(Font(R.font.notosanskr_medium)),
+    fontSize = 11.sp,
+    fontWeight = FontWeight(500),
+    lineHeight = 18.sp
+)
+val caption2_r_11 = TextStyle(
+    fontFamily = FontFamily(Font(R.font.notosanskr_regular)),
+    fontSize = 11.sp,
+    fontWeight = FontWeight(400),
+    lineHeight = 18.sp
+)
+val caption3_m_10 = TextStyle(
+    fontFamily = FontFamily(Font(R.font.notosanskr_medium)),
+    fontSize = 10.sp,
+    fontWeight = FontWeight(500),
+    lineHeight = 18.sp
+)
+val caption3_r_10 = TextStyle(
+    fontFamily = FontFamily(Font(R.font.notosanskr_regular)),
+    fontSize = 10.sp,
+    fontWeight = FontWeight(400),
+    lineHeight = 15.sp,
+    letterSpacing = 0.3.sp
+)
