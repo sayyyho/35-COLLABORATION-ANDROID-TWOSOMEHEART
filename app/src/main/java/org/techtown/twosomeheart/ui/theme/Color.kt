3125bc689b89105cabaec340bee5f2e586e720cd
hyeumm <20221159@sungshin.ac.kr>
2024-11-19 23:12:06 +0900

    setting/#9: color data class 추가

--- app/src/main/java/org/techtown/twosomeheart/ui/theme/Color.kt
+++ app/src/main/java/org/techtown/twosomeheart/ui/theme/Color.kt
@@ -1,14 +1,7 @@
 package org.techtown.twosomeheart.ui.theme
 
+import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.graphics.Color
-
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
 
 val Red10 = Color(0xFFFFF3F7)
 val Red20 = Color(0xFFFFDEE7)
@@ -34,3 +27,51 @@ val Black = Color(0xFF000000)
 
 val Red40_40 = Color(0x66D50037)
 val Red40_12 = Color(0x1FD50037)
+
+data class TwosomeColors(
+    val Red10: Color,
+    val Red20: Color,
+    val Red30: Color,
+    val Red40: Color,
+    val Orange: Color,
+    val Blue10: Color,
+    val Blue20: Color,
+    val White: Color,
+    val Gray10: Color,
+    val Gray20: Color,
+    val Gray30: Color,
+    val Gray40: Color,
+    val Gray50: Color,
+    val Gray60: Color,
+    val Gray70: Color,
+    val Gray80: Color,
+    val Gray90: Color,
+    val Black: Color,
+    val Red40_40: Color,
+    val Red40_12: Color,
+)
+
+val TwosomeHeartColors = TwosomeColors(
+    Red10 = Red10,
+    Red20 = Red20,
+    Red30 = Red30,
+    Red40 = Red40,
+    Orange = Orange,
+    Blue10 = Blue10,
+    Blue20 = Blue20,
+    White = White,
+    Gray10 = Gray10,
+    Gray20 = Gray20,
+    Gray30 = Gray30,
+    Gray40 = Gray40,
+    Gray50 = Gray50,
+    Gray60 = Gray60,
+    Gray70 = Gray70,
+    Gray80 = Gray80,
+    Gray90 = Gray90,
+    Black = Black,
+    Red40_40 = Red40_40,
+    Red40_12 = Red40_12
+)
+
+val LocalTwosomeColors = staticCompositionLocalOf { TwosomeHeartColors }
