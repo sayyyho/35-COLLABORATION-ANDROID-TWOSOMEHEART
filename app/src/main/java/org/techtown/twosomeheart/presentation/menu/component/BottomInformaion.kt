41e5ea33fc69752a898c13d28098200475ebd189
hyeumm <20221159@sungshin.ac.kr>
2024-11-20 16:09:16 +0900

    feature/#14: 메뉴 뷰 하단 주문 매장 컴포넌트 구현

---
+++ app/src/main/java/org/techtown/twosomeheart/presentation/menu/component/BottomInformaion.kt
@@ -0,0 +1,79 @@
+package org.techtown.twosomeheart.presentation.menu.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import org.techtown.twosomeheart.ui.theme.Black
+import org.techtown.twosomeheart.ui.theme.TwosomeHeartTheme
+import org.techtown.twosomeheart.ui.theme.TwosomeHeartTypography
+import org.techtown.twosomeheart.ui.theme.White
+
+@Composable
+fun BottomInformaion(
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .clip(shape = RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp))
+            .background(Black)
+            .padding(horizontal = 16.dp),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Column(
+            modifier = Modifier.padding(top = 20.dp, bottom = 14.dp)
+        ) {
+            Text(
+                "주문 매장",
+                style = TwosomeHeartTypography.caption1M12,
+                color = White
+            )
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    "안양호계금명점",
+                    style = TwosomeHeartTypography.body1B14,
+                    color = White
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+                Text(
+                    "349m",
+                    style = TwosomeHeartTypography.caption1B12,
+                    color = White
+                )
+            }
+        }
+        Text(
+            text = "변경",
+            style = TwosomeHeartTypography.body2R13,
+            modifier = Modifier
+                .padding(top = 23.dp, bottom = 20.dp)
+                .border(width = 1.dp, color = White)
+                .padding(vertical = 4.dp, horizontal = 11.dp),
+            color = White
+        )
+    }
+}
+
+@Preview
+@Composable
+fun BottomInformationPreview() {
+    TwosomeHeartTheme {
+        BottomInformaion()
+    }
+}
