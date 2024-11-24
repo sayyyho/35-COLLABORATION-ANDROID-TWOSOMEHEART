40642633bcef69ed134fbbe747797fde1e55b0e2
kangyein9892 <inwon9892@naver.com>
2024-11-25 04:31:21 +0900

    chore/#30: menu detail image coil 사용으로 수정

--- app/src/main/java/org/techtown/twosomeheart/presentation/detail/DetailRoute.kt
+++ app/src/main/java/org/techtown/twosomeheart/presentation/detail/DetailRoute.kt
@@ -9,7 +9,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
@@ -18,6 +20,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
@@ -28,6 +31,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
+import coil.compose.AsyncImage
 import org.techtown.twosomeheart.R
 import org.techtown.twosomeheart.core.component.BlackBottomButton
 import org.techtown.twosomeheart.core.component.Topbar
@@ -122,8 +126,9 @@ fun DetailScreen(
                 is UiState.Failure -> {}
 
                 is UiState.Success -> {
-                    Image(
-                        painter = painterResource(R.drawable.img_detail_banana_latte),
+
+                    AsyncImage(
+                        model = state.data.menuImageUrl,
                         contentDescription = stringResource(R.string.menu_detail_image),
                         modifier = Modifier.fillMaxWidth(),
                         contentScale = ContentScale.Crop
@@ -186,7 +191,7 @@ fun DetailScreenPreview() {
                         "카페인(mg/%): 92"
                     ),
                     menuAllergy = "우유",
-                    menuImageUrl = "dummy"
+                    menuImageUrl = "https://private-user-images.githubusercontent.com/69308068/389300533-8d353883-fb4b-4608-b0b4-24d2bc074133.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MzI0NzY4OTMsIm5iZiI6MTczMjQ3NjU5MywicGF0aCI6Ii82OTMwODA2OC8zODkzMDA1MzMtOGQzNTM4ODMtZmI0Yi00NjA4LWIwYjQtMjRkMmJjMDc0MTMzLnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNDExMjQlMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjQxMTI0VDE5Mjk1M1omWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPTI5MTRlNGNjODE0ODJkYWI3YzgwZmU4NDFkNTBkMTE2MmUyYTkwZDBhMmQwZTZiNzhkZDZkOTVmYzYwZTlhZDgmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0In0.IMOWSpla8sBIJZzTXv3oyo8AmecUl8uyim89XBdI1TM"
                 )
             )
         )
--- app/src/main/java/org/techtown/twosomeheart/presentation/detail/DetailViewModel.kt
+++ app/src/main/java/org/techtown/twosomeheart/presentation/detail/DetailViewModel.kt
@@ -30,7 +30,7 @@ class DetailViewModel: ViewModel() {
             "1회 제공량: 325ml\n총 제공량: 1잔\n열량(Kcal): 260\n당류(g/%): 30/30\n단백질(g/%): 7/13\n포화지방(g/%): 5/33\n나트륨(mg/%): 160/8\n카페인(mg/%): 92"
         ),
         menuAllergy = "우유",
-        menuImageUrl = "dummy"
+        menuImageUrl = "https://private-user-images.githubusercontent.com/69308068/389300533-8d353883-fb4b-4608-b0b4-24d2bc074133.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MzI0NzY4OTMsIm5iZiI6MTczMjQ3NjU5MywicGF0aCI6Ii82OTMwODA2OC8zODkzMDA1MzMtOGQzNTM4ODMtZmI0Yi00NjA4LWIwYjQtMjRkMmJjMDc0MTMzLnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNDExMjQlMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjQxMTI0VDE5Mjk1M1omWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPTI5MTRlNGNjODE0ODJkYWI3YzgwZmU4NDFkNTBkMTE2MmUyYTkwZDBhMmQwZTZiNzhkZDZkOTVmYzYwZTlhZDgmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0In0.IMOWSpla8sBIJZzTXv3oyo8AmecUl8uyim89XBdI1TM"
     )
 
     private fun splitNutritionText(nutritionTexts: String): List<String>? {
