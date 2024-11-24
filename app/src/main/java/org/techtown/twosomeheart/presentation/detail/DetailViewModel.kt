c299796475117d82040f5dc96af9ef33d6163435
kangyein9892 <inwon9892@naver.com>
2024-11-25 03:15:57 +0900

    chore/#30: DetailViewmodel dummy data 가격 Int로 수정

--- app/src/main/java/org/techtown/twosomeheart/presentation/detail/DetailViewModel.kt
+++ app/src/main/java/org/techtown/twosomeheart/presentation/detail/DetailViewModel.kt
@@ -24,7 +24,7 @@ class DetailViewModel: ViewModel() {
         menuName = "바나나 샷 라떼",
         menuStatus = "New",
         menuDescription = "달콤한 바나나 라떼에 에스프레소 샷 추가!",
-        menuPrice = "5,500",
+        menuPrice = 5500,
         menuCaution = "고카페인, 우유",
         menuNutrition = "1회 제공량: 325ml\n총 제공량: 1잔\n열량(Kcal): 260\n당류(g/%): 30/30\n단백질(g/%): 7/13\n포화지방(g/%): 5/33\n나트륨(mg/%): 160/8\n카페인(mg/%): 92",
         menuAllergy = "우유",
