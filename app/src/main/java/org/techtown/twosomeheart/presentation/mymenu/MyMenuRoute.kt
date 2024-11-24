82045fc32c80fd982cc4da9198c323cf01ebc0d7
sayyyho <323psh@naver.com>
2024-11-24 18:10:10 +0900

    feature/#31: MyMenuScreen 상단 전체선택 및 선택삭제 UI 구현

---
+++ app/src/main/java/org/techtown/twosomeheart/presentation/mymenu/MyMenuRoute.kt
@@ -0,0 +1,172 @@
+package org.techtown.twosomeheart.presentation.mymenu
+
+import android.inputmethodservice.Keyboard
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
+import kotlinx.collections.immutable.PersistentList
+import kotlinx.collections.immutable.persistentListOf
+import org.techtown.twosomeheart.R
+import org.techtown.twosomeheart.core.component.Topbar
+import org.techtown.twosomeheart.core.extension.noRippleClickable
+import org.techtown.twosomeheart.core.util.UiState
+import org.techtown.twosomeheart.presentation.menu.MenuViewModel
+import org.techtown.twosomeheart.presentation.menu.model.MenuModel
+import org.techtown.twosomeheart.ui.theme.TwosomeHeartColors
+import org.techtown.twosomeheart.ui.theme.TwosomeHeartTheme
+import org.techtown.twosomeheart.ui.theme.TwosomeHeartTypography
+import org.techtown.twosomeheart.ui.theme.White
+
+@Composable
+fun MyMenuRoute(
+    paddingValues: PaddingValues,
+    navigateUp: () -> Unit,
+    viewModel: MenuViewModel = viewModel()
+) {
+    val state by viewModel.state.collectAsStateWithLifecycle()
+
+    LaunchedEffect(true) {
+        viewModel.getMenuDummy()
+    }
+
+    MyMenuScreen(
+        paddingValues = paddingValues,
+        navigateUp = navigateUp,
+        state = state.uiState
+    )
+}
+
+@Composable
+fun MyMenuScreen(
+    paddingValues: PaddingValues,
+    navigateUp: () -> Unit,
+    state: UiState<PersistentList<MenuModel>>,
+    modifier: Modifier = Modifier
+) {
+    Box(
+        modifier = modifier
+            .padding(paddingValues)
+            .background(White)
+            .fillMaxSize()
+    ) {
+        Column {
+            Topbar(
+                leadingIcon = {
+                    Icon(
+                        imageVector = ImageVector.vectorResource(R.drawable.ic_back),
+                        contentDescription = ""
+                    )
+                },
+                leadingIcon2 = {
+                    Icon(
+                        imageVector = ImageVector.vectorResource(R.drawable.ic_home),
+                        contentDescription = ""
+                    )
+                },
+                text = "투썸오더",
+                trailingIcon = {
+                    Icon(
+                        imageVector = ImageVector.vectorResource(R.drawable.ic_mymenu_plus),
+                        contentDescription = ""
+                    )
+                },
+            )
+            Text(
+                text = "총 3개",
+                style = TwosomeHeartTypography.caption1R12Tight,
+                color = TwosomeHeartColors.Gray90,
+                modifier = Modifier.padding(start = 16.dp, top = 23.dp)
+            )
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 16.dp, top = 10.dp, end = 17.dp, bottom = 13.dp),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Row(
+                    horizontalArrangement = Arrangement.spacedBy(5.dp, Alignment.Start),
+                    verticalAlignment = Alignment.CenterVertically,
+                ) {
+                    Icon(
+                        imageVector = ImageVector.vectorResource(R.drawable.ic_checkbox_diselect),
+                        contentDescription = "",
+                        tint = Color.Unspecified,
+                    )
+                    Text(
+                        text = "전체선택",
+                        style = TwosomeHeartTypography.title1R16,
+                        color = TwosomeHeartColors.Gray90
+                    )
+                }
+                Text(
+                    text = "선택삭제",
+                    style = TwosomeHeartTypography.caption2R11.copy(
+                        textDecoration = TextDecoration.Underline
+                    ),
+                    color = TwosomeHeartColors.Gray90,
+                )
+            }
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(1.dp)
+                    .padding(
+                        start = 16.dp,
+                        end = 1.dp
+                    )
+                    .border(width = 1.dp, color = Color.Black)
+            )
+        }
+    }
+
+}
+
+
+@Preview
+@Composable
+fun MyMenuScreenPreview() {
+    TwosomeHeartTheme {
+        MyMenuScreen(
+            paddingValues = PaddingValues(),
+            navigateUp = {},
+            state = UiState.Success(
+                persistentListOf(
+                    MenuModel(
+                        menuName = "바나나 샷 라떼",
+                        menuPrice = 5500,
+                        menuImage = R.drawable.img_menu_banana_latte,
+                    ),
+                    MenuModel(
+                        menuName = "바나나 샷 아메리카노",
+                        menuPrice = 5800,
+                        menuImage = R.drawable.img_menu_banana_ameicano,
+                    ),
+                )
+            )
+        )
+    }
+}
---
+++ app/src/main/java/org/techtown/twosomeheart/presentation/mymenu/MyMenuState.kt
@@ -0,0 +1,9 @@
+package org.techtown.twosomeheart.presentation.menu
+
+import kotlinx.collections.immutable.PersistentList
+import org.techtown.twosomeheart.core.util.UiState
+import org.techtown.twosomeheart.presentation.mymenu.model.MyMenuModel
+
+data class MyMenuState(
+    val uiState: UiState<PersistentList<MyMenuModel>> = UiState.Loading
+)
