7b3f2723ab0c428cf1c1ff8f876f8197388b2d77
kangyein9892 <inwon9892@naver.com>
2024-11-25 01:07:17 +0900

    feature/#30: MenuDetauilContent (메뉴 상단에 있는 정보) ui 구현

---
+++ app/src/main/java/org/techtown/twosomeheart/presentation/detail/component/MenuDetailContent.kt
@@ -0,0 +1,70 @@
+package org.techtown.twosomeheart.presentation.detail.component
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import org.techtown.twosomeheart.core.component.MenuStatusChip
+import org.techtown.twosomeheart.ui.theme.Black
+import org.techtown.twosomeheart.ui.theme.TwosomeHeartTypography
+
+@Composable
+fun MenuDetailContent(
+    isBestMenu: Boolean,
+    menuName: String,
+    menuDescription: String,
+    menuPrice: String,
+    modifier: Modifier = Modifier
+){
+    Column(
+        modifier = modifier
+            .padding(start = 16.dp)
+    ) {
+        MenuStatusChip(isBestMenu = isBestMenu)
+
+        Spacer(modifier = Modifier.height(7.dp))
+
+        Text(
+            text = menuName,
+            style = TwosomeHeartTypography.head3B20,
+            color = Black
+        )
+
+        Spacer(modifier = Modifier.height(5.dp))
+
+        Text(
+            text = menuDescription,
+            style = TwosomeHeartTypography.body1R14,
+            color = Black,
+            modifier = Modifier.padding(start = 6.dp)
+        )
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        MenuDetailPriceText(
+            price = menuPrice
+        )
+
+        Row(
+            modifier = Modifier
+                .fillMaxHeight(),
+            horizontalArrangement = Arrangement.spacedBy(4.5.dp)
+        ) {
+            // TODO: menu caution 처리 어떻게 할건지 생각
+            MenuCautionChip(text = "고카페인")
+            MenuCautionChip(
+                text = "우유",
+                isAllergen = true
+            )
+        }
+
+        Spacer(modifier = Modifier.height(22.dp))
+    }
+}
---
+++ app/src/main/java/org/techtown/twosomeheart/presentation/detail/component/MenuDetailPriceText.kt
@@ -0,0 +1,37 @@
+package org.techtown.twosomeheart.presentation.detail.component
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import org.techtown.twosomeheart.R
+import org.techtown.twosomeheart.ui.theme.Black
+import org.techtown.twosomeheart.ui.theme.TwosomeHeartTypography
+
+@Composable
+fun MenuDetailPriceText(
+    price: String,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier
+            .fillMaxHeight()
+    ) {
+        Text(
+            text = price,
+            style = TwosomeHeartTypography.head1B24,
+            color = Black,
+            modifier = Modifier.padding(vertical = 8.5.dp)
+        )
+        Text(
+            text = stringResource(R.string.menu_detail_price_won),
+            style = TwosomeHeartTypography.head4B18,
+            color = Black,
+            modifier = Modifier.padding(top = 5.dp)
+        )
+    }
+}
