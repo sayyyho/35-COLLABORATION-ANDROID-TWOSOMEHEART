889aac0da604ce43ff1c614c69065012ba907dd6
kangyein9892 <inwon9892@naver.com>
2024-11-25 01:12:55 +0900

    feature/#30: DetailModel 추가

---
+++ app/src/main/java/org/techtown/twosomeheart/presentation/detail/model/DetailModel.kt
@@ -0,0 +1,13 @@
+package org.techtown.twosomeheart.presentation.detail.model
+
+data class DetailModel(
+    val menuId: Long,
+    val menuName: String,
+    val menuStatus: String,
+    val menuDescription: String,
+    val menuPrice: String,
+    val menuCaution: String?,
+    val menuNutrition: String,
+    val menuAllergy: String?,
+    val menuImageUrl: String
+)
