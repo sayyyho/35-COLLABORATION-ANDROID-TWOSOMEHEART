5f6bda45377d3139fc1dab1ece320917b59d0a49
hyeumm <20221159@sungshin.ac.kr>
2024-11-27 18:16:46 +0900

    feature/#26: OptionRoute 구현

---
+++ app/src/main/java/org/techtown/twosomeheart/presentation/option/OptionRoute.kt
@@ -0,0 +1,247 @@
+package org.techtown.twosomeheart.presentation.option
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
+import org.techtown.twosomeheart.R
+import org.techtown.twosomeheart.core.component.Topbar
+import org.techtown.twosomeheart.core.extension.noRippleClickable
+import org.techtown.twosomeheart.presentation.option.component.BottomTotalPrice
+import org.techtown.twosomeheart.presentation.option.component.OptionItem
+import org.techtown.twosomeheart.presentation.option.component.OptionQuantityRow
+import org.techtown.twosomeheart.presentation.option.component.TwoOptionTab
+import org.techtown.twosomeheart.presentation.option.model.Option
+import org.techtown.twosomeheart.presentation.option.model.OptionType
+import org.techtown.twosomeheart.ui.theme.TwosomeHeartTheme
+import org.techtown.twosomeheart.ui.theme.White
+
+@Composable
+fun OptionRoute(
+    paddingValues: PaddingValues,
+    navigateUp: () -> Unit,
+    viewModel: OptionViewModel = viewModel()
+) {
+    val state by viewModel.state.collectAsStateWithLifecycle()
+
+    LaunchedEffect(
+        state.shotQuantity,
+        state.vanilaSyrupQuantity,
+        state.caramelSyrupQuantity,
+        state.hazelnutsSyrupQuantity,
+        state.creamQuantity,
+        state.caramelDrizzleQuantity,
+        state.chocolateDrizzleQuantity
+    ) {
+        viewModel.calculateTotalPrice()
+    }
+
+    OptionScreen(
+        paddingValues = paddingValues,
+        navigateUp = navigateUp,
+        onClickPlusButton = viewModel::plusOptionQuantity,
+        onClickMinusButton = viewModel::minusOptionQuantity,
+        isExpanded = { option -> state.expandedOption == option },
+        onClickExpandButton = viewModel::updateExpandedOption,
+        shotQuantity = state.shotQuantity,
+        vanilaSyrupQuantity = state.vanilaSyrupQuantity,
+        caramelSyrupQuantity = state.caramelSyrupQuantity,
+        hazelnutsSyrupQuantity = state.hazelnutsSyrupQuantity,
+        creamQuantity = state.creamQuantity,
+        caramelDrizzleQuantity = state.caramelDrizzleQuantity,
+        chocolateDrizzleQuantity = state.chocolateDrizzleQuantity,
+        totalPrice = state.totalPrice,
+        resetTotalPrice = viewModel::resetTotalPrice
+    )
+}
+
+@Composable
+fun OptionScreen(
+    paddingValues: PaddingValues,
+    navigateUp: () -> Unit,
+    onClickMinusButton: (OptionType, String) -> Unit,
+    onClickPlusButton: (OptionType, String) -> Unit,
+    isExpanded: (Option) -> Boolean,
+    onClickExpandButton: (Option) -> Unit,
+    resetTotalPrice: () -> Unit,
+    modifier: Modifier = Modifier,
+    shotQuantity: Int = 0,
+    vanilaSyrupQuantity: Int = 0,
+    caramelSyrupQuantity: Int = 0,
+    hazelnutsSyrupQuantity: Int = 0,
+    creamQuantity: Int = 0,
+    caramelDrizzleQuantity: Int = 0,
+    chocolateDrizzleQuantity: Int = 0,
+    totalPrice: Int = 0,
+) {
+    val scrollState = rememberScrollState()
+
+    Column(
+        modifier = modifier
+            .padding(paddingValues)
+            .fillMaxSize()
+            .background(White)
+    ) {
+        Topbar(
+            text = stringResource(R.string.option_personal_option),
+            trailingIcon = {
+                Icon(
+                    imageVector = ImageVector.vectorResource(R.drawable.ic_option_x),
+                    contentDescription = stringResource(R.string.option_close),
+                    modifier = Modifier.noRippleClickable(onClick = navigateUp)
+                )
+            }
+        )
+
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .weight(1f)
+        ) {
+            Column(
+                modifier = Modifier.verticalScroll(scrollState)
+            ) {
+                OptionItem(
+                    option = Option.SHOT,
+                    optionType = OptionType.SHOT,
+                    optionTypeQuantity = shotQuantity,
+                    isExpanded = isExpanded(Option.SHOT),
+                    updateExpandedState = { onClickExpandButton(Option.SHOT) },
+                    option1 = {
+                        TwoOptionTab(
+                            R.string.option_density,
+                            R.string.option_normal,
+                            R.string.option_lightly
+                        )
+                    },
+                    option2 = {
+                        OptionQuantityRow(
+                            optionType = OptionType.SHOT,
+                            onClickMinusButton = { onClickMinusButton(OptionType.SHOT, "샷") },
+                            onClickPlusButton = { onClickPlusButton(OptionType.SHOT, "샷") },
+                            optionTypeQuantity = shotQuantity
+                        )
+                    }
+                )
+
+                OptionItem(
+                    option = Option.SYRUP,
+                    optionType = OptionType.SYRUP,
+                    optionTypeQuantity = vanilaSyrupQuantity + caramelSyrupQuantity + hazelnutsSyrupQuantity,
+                    isExpanded = isExpanded(Option.SYRUP),
+                    updateExpandedState = { onClickExpandButton(Option.SYRUP) },
+                    option1 = {
+                        OptionQuantityRow(
+                            optionType = OptionType.SYRUP,
+                            onClickMinusButton = { onClickMinusButton(OptionType.SYRUP, "바닐라시럽") },
+                            onClickPlusButton = { onClickPlusButton(OptionType.SYRUP, "바닐라시럽") },
+                            optionTypeQuantity = vanilaSyrupQuantity
+                        )
+                    },
+                    option2 = {
+                        OptionQuantityRow(
+                            optionType = OptionType.SYRUP,
+                            onClickMinusButton = { onClickMinusButton(OptionType.SYRUP, "헤이즐넛시럽") },
+                            onClickPlusButton = { onClickPlusButton(OptionType.SYRUP, "헤이즐넛시럽") },
+                            optionTypeQuantity = hazelnutsSyrupQuantity
+                        )
+                    },
+                    option3 = {
+                        OptionQuantityRow(
+                            optionType = OptionType.SYRUP,
+                            onClickMinusButton = { onClickMinusButton(OptionType.SYRUP, "캬라멜시럽") },
+                            onClickPlusButton = { onClickPlusButton(OptionType.SYRUP, "캬라멜시럽") },
+                            optionTypeQuantity = caramelSyrupQuantity
+                        )
+                    }
+                )
+
+                OptionItem(
+                    option = Option.CREAM,
+                    optionType = OptionType.CREAM,
+                    optionTypeQuantity = creamQuantity,
+                    isExpanded = isExpanded(Option.CREAM),
+                    updateExpandedState = { onClickExpandButton(Option.CREAM) },
+                    option1 = {
+                        OptionQuantityRow(
+                            optionType = OptionType.CREAM,
+                            onClickMinusButton = { onClickMinusButton(OptionType.CREAM, "휘핑크림") },
+                            onClickPlusButton = { onClickPlusButton(OptionType.CREAM, "휘핑크림") },
+                            optionTypeQuantity = creamQuantity
+                        )
+                    }
+                )
+
+                OptionItem(
+                    option = Option.DRIZZLE,
+                    optionType = OptionType.DRIZZLE,
+                    optionTypeQuantity = caramelDrizzleQuantity + chocolateDrizzleQuantity,
+                    isExpanded = isExpanded(Option.DRIZZLE),
+                    updateExpandedState = { onClickExpandButton(Option.DRIZZLE) },
+                    option1 = {
+                        OptionQuantityRow(
+                            optionType = OptionType.DRIZZLE,
+                            onClickMinusButton = {
+                                onClickMinusButton(
+                                    OptionType.DRIZZLE,
+                                    "캬라멜드리즐"
+                                )
+                            },
+                            onClickPlusButton = { onClickPlusButton(OptionType.DRIZZLE, "캬라멜드리즐") },
+                            optionTypeQuantity = caramelDrizzleQuantity
+                        )
+                    },
+                    option2 = {
+                        OptionQuantityRow(
+                            optionType = OptionType.DRIZZLE,
+                            onClickMinusButton = {
+                                onClickMinusButton(
+                                    OptionType.DRIZZLE,
+                                    "초콜릿드리즐"
+                                )
+                            },
+                            onClickPlusButton = { onClickPlusButton(OptionType.DRIZZLE, "초콜릿드리즐") },
+                            optionTypeQuantity = chocolateDrizzleQuantity
+                        )
+                    }
+                )
+            }
+        }
+
+        BottomTotalPrice(
+            totalPrice = totalPrice,
+            onClickResetButton = resetTotalPrice,
+        )
+    }
+}
+
+@Preview
+@Composable
+fun OptionScreenPreview() {
+    TwosomeHeartTheme {
+        OptionScreen(
+            paddingValues = PaddingValues(),
+            navigateUp = {},
+            onClickPlusButton = { _, _ -> },
+            onClickMinusButton = { _, _ -> },
+            isExpanded = { _ -> false },
+            onClickExpandButton = {},
+            resetTotalPrice = {}
+        )
+    }
+}
