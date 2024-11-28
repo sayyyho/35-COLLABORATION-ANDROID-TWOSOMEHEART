b38e2e5c3c6365fe98ffc35d110ddabf262d24e1
hyeumm <20221159@sungshin.ac.kr>
2024-11-28 16:53:42 +0900

    chore/#40: menuId 적용 자동화로 수정

--- app/src/main/java/org/techtown/twosomeheart/data/service/TwosomeService.kt
+++ app/src/main/java/org/techtown/twosomeheart/data/service/TwosomeService.kt
@@ -1,6 +1,7 @@
 package org.techtown.twosomeheart.data.service
 
 import org.techtown.twosomeheart.data.dto.response.ResponseMenuDetailDto
+import org.techtown.twosomeheart.data.dto.response.ResponseMyMenuDto
 import retrofit2.http.GET
 import retrofit2.http.Path
 
@@ -8,10 +9,10 @@ interface TwosomeService {
     //menu detail
     @GET("api/v1/menu/{menuId}")
     suspend fun getMenuLists(
-        @Path("menuId") menuId: Int = 2
+        @Path("menuId") menuId: Long
     ): ResponseMenuDetailDto
-  
-  
+
+
     @GET("api/v1/likes")
     suspend fun getMyMenu(): ResponseMyMenuDto
 }
