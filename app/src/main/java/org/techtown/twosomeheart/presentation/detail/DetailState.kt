ff9dce77888eae351f669d25f94438021c1eb87a
kangyein9892 <inwon9892@naver.com>
2024-11-28 21:02:59 +0900

    feature/#45: detailRoute, detailScreen에 연결

--- app/src/main/java/org/techtown/twosomeheart/presentation/detail/DetailState.kt
+++ app/src/main/java/org/techtown/twosomeheart/presentation/detail/DetailState.kt
@@ -4,5 +4,6 @@ import org.techtown.twosomeheart.core.util.UiState
 import org.techtown.twosomeheart.presentation.detail.model.DetailModel
 
 data class DetailState(
-    val uiState: UiState<DetailModel> = UiState.Loading
+    val uiState: UiState<DetailModel> = UiState.Loading,
+    val isShowBottomSheet: Boolean = false
 )
