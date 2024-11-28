64d1567c92f1fbfc5eb24fdf9ec43294ea7aa685
kangyein9892 <inwon9892@naver.com>
2024-11-28 20:59:04 +0900

    feature/#45: menu like api 함수 구현

--- app/src/main/java/org/techtown/twosomeheart/data/service/TwosomeService.kt
+++ app/src/main/java/org/techtown/twosomeheart/data/service/TwosomeService.kt
@@ -1,8 +1,13 @@
 package org.techtown.twosomeheart.data.service
 
 import org.techtown.twosomeheart.data.dto.response.ResponseMenuDetailDto
+import org.techtown.twosomeheart.data.dto.request.RequestMenuLikeDto
+import org.techtown.twosomeheart.data.dto.response.ResponseMenuLikeDto
+import org.techtown.twosomeheart.data.dto.response.ResponseMyMenuDto
+import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.POST
 
 interface TwosomeService {
     //menu detail
@@ -10,8 +15,13 @@ interface TwosomeService {
     suspend fun getMenuLists(
         @Path("menuId") menuId: Int = 2
     ): ResponseMenuDetailDto
-  
-  
+
     @GET("api/v1/likes")
     suspend fun getMyMenu(): ResponseMyMenuDto
+
+    @POST("api/v1/menu/{menuId}/likes")
+    suspend fun likeMenu(
+        @Path("menuId") menuId: Long,
+        @Body requestBody: RequestMenuLikeDto
+    ): ResponseMenuLikeDto
 }
